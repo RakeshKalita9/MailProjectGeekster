1e97327c3fe8db084a44596dd2446acb06e9470f
Rakesh Kalita <rakeshkalita750@gmail.com>
2023-04-15 16:53:35 +0530

    first_Commit

---
+++ SendingMailJava/src/main/java/org/example/MailConstants.java
@@ -0,0 +1,14 @@
+package org.example;
+public class MailConstants {
+
+    public static final String SENDER = "rakesh.kalita259@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAILADDRESS = "rakeshkalita0161@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 12-April-2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This message is for security check...!!!";
+}
